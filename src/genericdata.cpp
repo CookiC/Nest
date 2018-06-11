d36925d5fc253deffb43f5c7f1e20279c5c6bf9d
CookiC <shen_yu_xiang@126.com>
2018-06-11 19:42:36 +0800

    Ash
    Version 1.6

--- src/genericdata.cpp
+++ src/genericdata.cpp
@@ -18,20 +18,14 @@ GenericData::GenericData(){
     p->left = p;
     p->right = p;
     head = p;
+    numCol = 0;
+    numRow = 0;
 }
 
 GenericData::~GenericData(){
-    for(auto p:colHead){
-        while(p->up!=p)
-            p->deleteUp();
-    }
-    while(head->up!=head)
-        head->deleteUp();
-    while(head->left!=head)
-        head->deleteLeft();
 }
 
-void GenericData::appendRowHead(const string rowName){
+void GenericData::appendRowHead(const string& rowName){
     Node* p = new Node(rowName);
     p->left = p;
     p->right = p;
@@ -41,18 +35,6 @@ void GenericData::appendRowHead(const string rowName){
     head->up = p;
     rowHead.push_back(p);
     numRow=(int)rowHead.size();
-}
-
-void GenericData::appendColHead(const string colName){
-    Node* p = new Node(colName);
-    p->up = p;
-    p->down = p;
-    p->left = head->left;
-    p->right = head->right;
-    p->left->right = p;
-    head->left = p;
-    colHead.push_back(p);
-    numCol=(int)colHead.size();
 }
 
 void GenericData::deleteRowHead(int index){
@@ -66,18 +48,101 @@ void GenericData::deleteRowHead(int index){
     delete p;
 }
 
-bool GenericData::appendRow(const GenericList &row, const string name){
-    appendRowHead(name);
+void GenericData::deleteRow(int index){
+    Node *p,*q;
+    for(p=rowHead[index]->left;p!=rowHead[index];p=q){
+        q=p->left;
+        p->up->down=p->down;
+        p->down->up=p->up;
+        delete p;
+    }
+    deleteRowHead(index);
+}
+
+bool GenericData::deleteRow(const string& name){
+    for(int i=0;i<numCol;++i)
+        if(rowHead[i]->value==name){
+            deleteRow(i);
+            return true;
+        }
+    return true;
+}
+
+void GenericData::appendColHead(const string& name){
+    InsertColHead(numCol,name);
+}
+
+void GenericData::InsertColHead(int index, const string &name){
+    Node *q;
+    if(index<numCol)
+        q=colHead[index];
+    else if(index==numCol)
+        q=head;
+    else
+        return;
+    Node *p = new Node(name);
+    p->up = p;
+    p->down = p;
+    p->left = q->left;
+    p->right = q;
+    p->left->right = p;
+    p->right->left = p;
+    colHead.push_back(nullptr);
+    for(int i=numCol;i>index;--i)
+        colHead[i]=colHead[i-1];
+    colHead[index]=p;
+    numCol=(int)colHead.size();
+}
+
+void GenericData::deleteColHead(int index){
+    Node* p = colHead[index];
+    for(int i=index;i<numCol;++i)
+        colHead[i]=colHead[i+1];
+    colHead.pop_back();
+    numCol=(int)colHead.size();
+    p->left->right = p->right;
+    p->right->left = p->left;
+    delete p;
+}
+
+void GenericData::deleteCol(int index){
+    Node *p,*q;
+    for(p=colHead[index]->up;p!=colHead[index];p=q){
+        q=p->up;
+        p->left->right=p->right;
+        p->right->left=p->left;
+        delete p;
+    }
+    deleteColHead(index);
+}
+
+int GenericData::getColIndex(const string& name){
+    for(int i=0;i<numCol;++i)
+        if(colHead[i]->value==name)
+            return i;
+    return -1;
+}
+
+bool GenericData::deleteCol(const string& name){
+    int i=getColIndex(name);
+    if(i<0)
+        return false;
+    deleteCol(i);
+    return true;
+}
+
+bool GenericData::appendRow(const GenericList &row, const string& name){
     if(row.size()!=numCol)
         return false;
-    Node* q;
-    Node* p=*rowHead.rbegin();
+    appendRowHead(name);
+    Node *q,*U,*R,*L,*D;
+    Node *p=*rowHead.rbegin();
     for(auto s:row){
         q = new Node(s);
-        Node*& L = p->left;
-        Node*& R = p;
-        Node*& U = L->up->right;
-        Node*& D = L->down->right;
+        L = p->left;
+        R = p;
+        U = L->up->right;
+        D = L->down->right;
         q->right = R;
         q->left = L;
         q->up = U;
@@ -87,10 +152,31 @@ bool GenericData::appendRow(const GenericList &row, const string name){
         U->down = q;
         D->up = q;
     }
+    return true;
 }
 
-void GenericData::deleteRow(int i){
-
+bool GenericData::insertCol(int index, const GenericList &col, const string &name){
+    if(col.size()!=numRow)
+        return false;
+    InsertColHead(index, name);
+    Node *q,*U,*R,*L,*D;
+    Node *p=colHead[index];
+    for(auto s:col){
+        q = new Node(s);
+        U = p->up;
+        D = p;
+        L = U->left->down;
+        R = U->right->down;
+        q->right = R;
+        q->left = L;
+        q->up = U;
+        q->down = D;
+        L->right = q;
+        R->left = q;
+        U->down = q;
+        D->up = q;
+    }
+    return true;
 }
 
 bool GenericData::loadCsv(string filePath, bool hasRowName, bool hasColName){
@@ -175,8 +261,49 @@ void GenericData::saveCsv(string filePath){
     }
 }
 
+void GenericData::colStrSplit(const string& name, const string& delimiter, bool repeat){
+    int i=getColIndex(name);
+    if(i<0)
+        return;
+    colStrSplit(i,delimiter);
+}
+
+void GenericData::colStrSplit(int index, const string& delimiter, bool repeat){
+    string name=colHead[index]->value;
+    bool canSplit;
+    Node *q,*p;
+    GenericList l;
+    int i,j,k=(int)delimiter.size();
+
+    i=0;
+    do{
+        canSplit=false;
+        l.clear();
+        p=colHead[i+index];
+        for(q=p->down;q!=p;q=q->down){
+           j=(int)q->value.find(delimiter);
+           if(j==string::npos)
+               l.push_back("");
+           else{
+               canSplit=true;
+               l.push_back(q->value.substr(0,j));
+               q->value.erase(0,j+k);
+           }
+        }
+        if(canSplit){
+            insertCol(i+index,l,name+'#'+std::to_string(i));
+            ++i;
+        }
+    }while(canSplit&&repeat);
+    p->value=name+'#'+std::to_string(i);
+}
+
 void GenericData::test(){
     GenericData gData;
     gData.loadCsv("E:/scientific research/experiment/data/Titanic Machine Learning from Disaster/train.csv");
+    //gData.deleteCol("PassengerId");
+    gData.colStrSplit("Name",", ");
+    gData.colStrSplit("Name#1",". ");
+    gData.colStrSplit("Ticket"," ");
     gData.saveCsv("E:/scientific research/experiment/data/Titanic Machine Learning from Disaster/train1.csv");
 }
--- src/genericdata.h
+++ src/genericdata.h
@@ -11,8 +11,15 @@ public:
     ~GenericData();
     bool loadCsv(string, bool=0, bool=1);
     void saveCsv(string);
-    bool appendRow(const GenericList&, const string="");
+    bool appendRow(const GenericList&, const string& = "");
+    void colStrSplit(int, const string&, bool=false);
+    void colStrSplit(const string&, const string&, bool=false);
     void deleteRow(int);
+    void deleteCol(int);
+    bool deleteRow(const string&);
+    bool deleteCol(const string&);
+    bool insertCol(int, const GenericList&, const string& = "");
+    int getColIndex(const string&);
     static void test();
 
 private:
@@ -27,9 +34,12 @@ private:
     vector<Node*> colHead;
 
     static void deleteNode(Node*);
-    void appendRowHead(const string="");
+
+    void appendColHead(const string& = "");
+    void appendRowHead(const string& = "");
+    void deleteColHead(int);
     void deleteRowHead(int);
-    void appendColHead(const string="");
+    void InsertColHead(int, const string& = "");
 
 };
 
